17a36a6726c6d6a8eb2a9745c4a49f5d3066da79
duant95 <duant95@hotmail.com>
2025-02-25 17:50:17 -0600

    Agregada documentación con Swagger

--- src/main/java/com/example/anuncios_api/controller/AnuncioController.java
+++ src/main/java/com/example/anuncios_api/controller/AnuncioController.java
@@ -3,11 +3,15 @@ package com.example.anuncios_api.controller;
 import com.example.anuncios_api.model.Anuncio;
 import com.example.anuncios_api.service.AnuncioService;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/anuncios")  // Define el endpoint principal
+@Tag(name = "Anuncios", description = "API para gestionar anuncios de autos")  // Categoría en la doc
 public class AnuncioController {
     private final AnuncioService anuncioService;
 
@@ -15,8 +19,10 @@ public class AnuncioController {
         this.anuncioService = anuncioService;
     }
 
+    @Operation(summary = "Obtener anuncios", description = "Retorna una lista de anuncios, opcionalmente filtrando por modelo.")
     @GetMapping
-    public List<Anuncio> getAnuncios(@RequestParam(required = false) String modelo) {
+    public List<Anuncio> getAnuncios( @Parameter(description = "Modelo del auto para filtrar los anuncios") 
+    @RequestParam(required = false) String modelo) {
         List<Anuncio> anuncios = anuncioService.getAnuncios();
 
         // Si se pasa un modelo como parámetro, filtra los resultados
